efcb378b55a61d544c8644064e3573c386ce840a
ebuchanan9 <79753303+ebuchanan9@users.noreply.github.com>
2021-03-19 11:07:51 -0400

    added humidity innerHTMl

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ function displayTemperature(response){
     let cityElement = document.querySelector(`#city`);
     let temperatureElement = document.querySelector(`#temperature`);
     let descriptionElement = document. querySelector(`#description`);
+    let humidityElement = document.querySelector(`#humidity`);
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML=(response.data.name);
     descriptionElement.innerHTML = (response.data.weather[0].description);
+    humidityElement.innerHTML = (response.data.main.humidity);
 }
 
 let apiKey ="00ccd60e50147aa10fe0e380d859f398";

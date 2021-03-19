f3d23da1fe6e7496b78dc8b67718e776c821dc50
ebuchanan9 <79753303+ebuchanan9@users.noreply.github.com>
2021-03-19 11:14:58 -0400

    Added windspeed innerhtml

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ function displayTemperature(response){
     let temperatureElement = document.querySelector(`#temperature`);
     let descriptionElement = document. querySelector(`#description`);
     let humidityElement = document.querySelector(`#humidity`);
+    let windElement = document.querySelector(`#wind`);
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML=(response.data.name);
     descriptionElement.innerHTML = (response.data.weather[0].description);
     humidityElement.innerHTML = (response.data.main.humidity);
+    windElement.innerHTML= Math.round(response.data.wind.speed);
+
+    console.log(response.data);
 }
 
 let apiKey ="00ccd60e50147aa10fe0e380d859f398";

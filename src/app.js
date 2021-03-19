fd56130e0b81372054554ab0387255452b8a06d1
ebuchanan9 <79753303+ebuchanan9@users.noreply.github.com>
2021-03-19 10:57:43 -0400

    updated innerHTML for city and Current Temperature

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
-let apiKey =`00ccd60e50147aa10fe0e380d859f398`;
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+function displayTemperature(response){
+    let cityElement = document.querySelector(`#city`);
+    let temperatureElement = document.querySelector(`#temperature`);
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML=(response.data.name);
+}
+
+let apiKey ="00ccd60e50147aa10fe0e380d859f398";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=imperial`;
+
+
+axios.get(apiUrl).then(displayTemperature);
